2efda24568853c5d3be8583da9edacc77e348e70
wajid mughal <wajidmughalwm2001@gmail.com>
2026-01-21 23:04:21 +0500

    changes in astroconfig.mjs for trailingslash prevention

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://100xlift.com',
   integrations: [sitemap()],
+  trailingSlash: "never" // or "always" but be consistent
 });
